38aa23da9aa2c924528fef5983840c09125cf165
Ola Petersson <ola.petersson@meltwater.com>
2023-04-29 21:22:36 +0200

    format on save

--- after/plugin/mason-and-null-ls.lua
+++ after/plugin/mason-and-null-ls.lua
@@ -20,3 +20,11 @@ require("null-ls").setup({
         end
     end,
 })
+
+-- format on save --
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = { '*.tsx', '*.ts', '*.jsx', '*.js' },
+  command = 'silent! EslintFixAll',
+  group = vim.api.nvim_create_augroup('MyAutocmdsJavaScripFormatting', {}),
+})
+
