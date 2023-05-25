917966752d39a6dd6a8792511ab6da38905cab2d
Ola Petersson <ola.petersson@meltwater.com>
2023-05-25 08:34:38 +0200

    indent

--- lua/vim-and-keymaps.lua
+++ lua/vim-and-keymaps.lua
@@ -1,5 +1,5 @@
 -- !! vim settings !! --
-  vim.opt.swapfile = false
+vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
