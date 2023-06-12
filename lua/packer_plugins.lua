552c0181bcbf8c05c491aca22b987caf4fa75b87
Ola Petersson <ola.petersson@meltwater.com>
2023-06-12 08:32:51 +0200

    Don't think I ever used this one.

--- lua/packer_plugins.lua
+++ lua/packer_plugins.lua
@@ -140,17 +140,6 @@ return require('packer').startup(function(use)
     event = "InsertEnter",
   }
 
-  -- surround --
-  use({
-      "kylechui/nvim-surround",
-      tag = "*", -- Use for stability; omit to use `main` branch for the latest features
-      config = function()
-          require("nvim-surround").setup({
-              -- Configuration here, or leave empty to use defaults
-          })
-      end
-  })
-
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
